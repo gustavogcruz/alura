690d04c6491d082a54f1b6c5c05d4d7e82c952bc
Gustavo Cruz <gustavo.cruz@empresa1.com.br>
2024-08-14 18:37:36 -0300

    atualizando arquivos cursos

---
+++ 1-A-partir-do-zero-iniciante-em-programacao/0-LogicaProgramacao/0-logica-js-projeto_inicial/app.js
@@ -0,0 +1,26 @@
+alert('Bem vindo ao jogo do número secreto');
+let numeroMaximo = 1000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo) + 1;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+// enquanto chute não for igual ao número secreto
+while (chute != numeroSecreto) {    
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`o número secreto é menor que ${chute}`);
+        } else {
+            alert(`o número secreto é maior que ${chute}`);
+        }
+        tentativas++;
+    }
+}
+
+// operador ternários
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você acertou o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
