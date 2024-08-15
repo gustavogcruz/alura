ba205a1245521e7e884aec3e676a75a10998cb79
Gustavo Cruz <gustavo.cruz@empresa1.com.br>
2024-08-15 15:46:49 -0300

    adicionando arquivo curso

--- 1-A-partir-do-zero-iniciante-em-programacao/0-LogicaProgramacao/1-explore-funcoes-e-listas/app.js
+++ 1-A-partir-do-zero-iniciante-em-programacao/0-LogicaProgramacao/1-explore-funcoes-e-listas/app.js
@@ -1,3 +1,5 @@
+//https://eloquentjavascript.net/
+
 let listaNumerosSorteados = [];
 let numeroLimiteLista = 10;
 let numeroSecreto = gerarNumeroAleatorio();
@@ -13,6 +15,7 @@ function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do Número Secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
+
 exibirMensagemInicial();
 function verificarChute() {
     let chute = document.querySelector('input').value;
