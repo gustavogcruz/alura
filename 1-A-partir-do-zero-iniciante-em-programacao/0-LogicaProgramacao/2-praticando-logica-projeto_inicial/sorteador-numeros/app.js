75f2e227059649f8a79564ee5e9f2bc35107685d
Gustavo Cruz <gustavo.cruz@empresa1.com.br>
2024-08-15 18:04:08 -0300

    atualizando arquivos cursos

---
+++ 1-A-partir-do-zero-iniciante-em-programacao/0-LogicaProgramacao/2-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -0,0 +1,19 @@
+function sortear() {
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    let sorteados = [];
+    let numero;
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+        sorteados.push(numero);
+    }
+
+    alert(sorteados);
+
+}
+
+function obterNumeroAleatorio(min, max, quantidade) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;        
+}
